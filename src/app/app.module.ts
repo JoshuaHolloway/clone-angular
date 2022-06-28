f08228940881daf07a96da8529aa3db05eaacce0
JoshuaHolloway <jhollow6@asu.edu>
2022-06-28 15:07:22 -0500

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, MainNavComponent],
+  declarations: [AppComponent, HomeComponent, NavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
